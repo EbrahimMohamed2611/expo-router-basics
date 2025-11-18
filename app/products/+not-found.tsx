90063a4a2f55241edf1c61026e836540d8057953
Ebrahim ElRouby <EMKC0471@EmkanFinance.com.sa>
2025-11-19 00:12:38 +0200

    add dynamic routes and not found

---
+++ app/products/+not-found.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function NotFoundPage() {
+  return (
+    <View style={styles.container}>
+      <Text>Scoped NotFoundPage</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        gap:20
+
+    }
+})
---
+++ app/profile/index.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'expo-router'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Profile() {
+  return (
+    <View style={styles.conatiner}>
+      <Text>Profile</Text>
+      <Link href="/users/1">User 1</Link>
+      <Link href="/users/2">User 2</Link>
+      <Link href="/users/3">User 3</Link>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    conatiner:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
